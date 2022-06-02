adac1c4e437f92f2b5332af840007529fa4d6d00
Huilin Zhang <orangeade93626@gmail.com>
2022-06-01 22:26:10 -0700

    加了花房，transport基本完成

--- Assets/Scripts/Doors/DoorController.cs
+++ Assets/Scripts/Doors/DoorController.cs
@@ -14,26 +14,63 @@ public class DoorController : MonoBehaviour
         // var door_pos = dorm_door.transform.position;
         // Debug.Log(door_pos);
         Debug.Log(door.tag);
-        if(door.tag == "Start_door") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        else if(door.tag == "Lobby_door_up_left") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else if(door.tag == "Lobby_down_left") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        else if(door.tag == "Lobby_up_right") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-        }
-        else if(door.tag == "Secret_door") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else if(door.tag == "Data_secret") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        else if(door.tag == "Data_exit") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        // if(door.tag == "Start_door") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // }
+        // else if(door.tag == "Lobby_door_up_left") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // }
+        // else if(door.tag == "Lobby_down_left") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // }
+        // else if(door.tag == "Lobby_up_right") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        // }
+        // else if(door.tag == "Secret_door") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // }
+        // else if(door.tag == "Data_secret") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // }
+        // else if(door.tag == "Data_exit") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        // }
+        // else if(door.tag == "Working_exit") {
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // }
+        switch(door.tag) {
+            case "Start_door":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case "Lobby_door_up_left":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                break;
+            case "Lobby_down_left":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case "Lobby_up_right":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+                break;
+            case "Secret_door":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                break;
+            case "Data_secret":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case "Data_exit":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+                break;
+            case "Working_exit":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                break;
+            case "Plant_exit":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
+                break;
+            case "Plant_enter":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+                break;
+            default:
+                break;
         }
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         // var player_size = player.GetComponent<SpriteRenderer>().bounds.size;
