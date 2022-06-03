144e39f3c569db9cd721e85e72b29379390c72d2
Huilin Zhang <orangeade93626@gmail.com>
2022-06-03 15:24:11 -0700

    update sound

--- Assets/Scripts/Doors/DoorController.cs
+++ Assets/Scripts/Doors/DoorController.cs
@@ -11,40 +11,8 @@ public class DoorController : MonoBehaviour
     void OnMouseDown()
     {
         Debug.Log("dorm_door click!");
-        // var door_pos = dorm_door.transform.position;
-        // Debug.Log(door_pos);
         Debug.Log(door.tag);
         switch(door.tag) {
-            case "Start_door":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                break;
-            case "Lobby_door_up_left":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-                break;
-            case "Lobby_down_left":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                break;
-            case "Lobby_up_right":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-                break;
-            case "Secret_door":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-                break;
-            case "Data_secret":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                break;
-            case "Data_exit":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-                break;
-            case "Working_exit":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-                break;
-            case "Plant_exit":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
-                break;
-            case "Plant_enter":
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
-                break;
             case "Starting_game":
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 6);
                 break;
@@ -54,11 +22,6 @@ public class DoorController : MonoBehaviour
             default:
                 break;
         }
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        // var player_size = player.GetComponent<SpriteRenderer>().bounds.size;
-        // var player_pos = player.transform.position;
-        // Debug.Log("player pos: ");
-        // Debug.Log(player_pos);
     }
     private void changeScene()
     {
